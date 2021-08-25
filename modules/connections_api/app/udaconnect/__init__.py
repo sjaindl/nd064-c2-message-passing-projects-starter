60e563dc79429542a548584f76262c883afae1bc
Stefan Jaindl <stefan.jaindl@alturos.com>
2021-08-26 01:19:23 +0200

    Add connections microservice

--- modules/connections_api/app/udaconnect/__init__.py
+++ modules/connections_api/app/udaconnect/__init__.py
@@ -2,7 +2,7 @@ from app.udaconnect.models import Connection  # noqa
 from app.udaconnect.schemas import ConnectionSchema  # noqa
 
 
-def register_routes(api, app, root="connections-api"):
+def register_routes(api, app, root="api"):
     from app.udaconnect.controllers import api as udaconnect_api
 
     api.add_namespace(udaconnect_api, path=f"/{root}")
